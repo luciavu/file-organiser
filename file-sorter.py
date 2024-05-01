e71f7cf5d02ad9cb4f50765deaf5dc03ae14c5de
Lucia Vu <luciavu6@outlook.com>
2024-05-01 23:10:19 +1000

    Add lists of common file formats

--- file-sorter.py
+++ file-sorter.py
@@ -8,9 +8,21 @@ def sort_directory(user, directory):
         print(f"Error: Directory '{full_path}' does not exist.")
         return
     
+    # Change current working directory to chosen
     os.chdir(full_path)  
-    for file in os.listdir():
-        print(file)
+
+    # Lists of common file formats
+    documents = [".doc", ".docx", ".docm" ".html", ".htm", ".odt", ".pdf", 
+                 ".xls", ".xlsx", ".rtf", ".ppt", ".pptx", ".txt", ".psd", ".csv"]
+    images = [".jpeg", ".jpg", ".png", ".gif", ".tiff", ".ico", ".raw"]
+    videos = [".mp4", ".mov", ".wmv", ".avi", ".avchd", ".flv", ".f4v", ".swf",
+               ".mkv", ".webm"]
+    music = [".mp3", ".wav", ".flac", ".m4a"]
+    applications = [".exe", ".lnk"]
+    archives = [".zip", ".rar", ".7z", ".tar", ".gz"]
+    code = [".c", ".py", ".java", ".cpp", ".js", ".html", ".css", ".php"]
+
+        
 
 
 def welcome_message():
