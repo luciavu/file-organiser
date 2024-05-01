e5c837de358f36497d237184307a1118c724b623
Lucia Vu <luciavu6@outlook.com>
2024-05-01 23:31:21 +1000

    Create sorting folders and fix welcome message

--- file-sorter.py
+++ file-sorter.py
@@ -12,31 +12,43 @@ def sort_directory(user, directory):
     os.chdir(full_path)  
 
     # Lists of common file formats
-    documents = [".doc", ".docx", ".docm" ".html", ".htm", ".odt", ".pdf", 
-                 ".xls", ".xlsx", ".rtf", ".ppt", ".pptx", ".txt", ".psd", ".csv"]
-    images = [".jpeg", ".jpg", ".png", ".gif", ".tiff", ".ico", ".raw"]
-    videos = [".mp4", ".mov", ".wmv", ".avi", ".avchd", ".flv", ".f4v", ".swf",
-               ".mkv", ".webm"]
-    music = [".mp3", ".wav", ".flac", ".m4a"]
-    applications = [".exe", ".lnk"]
-    archives = [".zip", ".rar", ".7z", ".tar", ".gz"]
-    code = [".c", ".py", ".java", ".cpp", ".js", ".html", ".css", ".php"]
+    file_extensions = {
+        "Documents": [".doc", ".docx", ".docm" ".html", ".htm", ".odt", ".pdf", 
+                     ".xls", ".xlsx", ".rtf", ".ppt", ".pptx", ".txt", ".psd", ".csv"],
+        "Images": [".jpeg", ".jpg", ".png", ".gif", ".tiff", ".ico", ".raw"],
+        "Videos": [".mp4", ".mov", ".wmv", ".avi", ".avchd", ".flv", ".f4v", ".swf",
+                   ".mkv", ".webm"],
+        "Music" :[".mp3", ".wav", ".flac", ".m4a"],
+        "Applications": [".exe", ".lnk"],
+        "Archives" : [".zip", ".rar", ".7z", ".tar", ".gz"],
+        "Code": [".c", ".py", ".java", ".cpp", ".js", ".html", ".css", ".php", ".ipynb"],
+        "Other" : []
+    }
 
-        
+    print("Sorting files...")
+
+    # Create folders to sort files into if not exist already
+    for folder in file_extensions:
+        folder_path = os.path.join(full_path, folder)
+        os.makedirs(folder_path, exist_ok=True)
+    return
+
+    #for file in os.listdir():
+
+    print("Done!")
 
 
 def welcome_message():
+    print("-" * 50)
     print("Welcome to File Sorter!")
     print("This program helps you organise files on your desktop.")
     print("Please choose a directory path to clean up.")
-    print("""Examples of directory paths:
-          Downloads
-          Documents
-          Documents/folder_name\n""")
+    print("\nExamples of directory paths:\nDownloads\nDocuments" + r"\folder_name")
+    print(r"OneDrive\Documents\folder_name")
 
 def main():
     welcome_message()
-    directory = input("Enter directory path: ")
+    directory = input("\nEnter directory path: ")
     user = input("Enter username: ")
     sort_directory(user, directory)
 
